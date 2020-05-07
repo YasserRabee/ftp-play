74c893a3e41c1e543c30e2ba71c9457cc8b97ead
Yasser Rabee <yasserrabee@gmail.com>
2020-05-07 21:33:17 +0200

    add server to the docker image

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ FROM python:3.6
 WORKDIR /app
 
 ADD client.py /app
+ADD server.py /app
 
-ENV SOURCE_ADDRESS='35.224.32.114'
 ENV SOURCE_ADDRESS_PORT='2123'
 
-ENTRYPOINT ["python", "client.py"]
+ENTRYPOINT ["python"]
+CMD ["client.py"]
--- server.py
+++ server.py
@@ -4,6 +4,12 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
+
+class VerboseServer(FTPServer):
+    def handle_accepted(self, sock, addr):
+        self.log_info("connection from: %s:%s" % addr)
+
+
 def main():
     # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = DummyAuthorizer()
@@ -22,12 +28,12 @@ def main():
 
     # Specify a masquerade address and the range of ports to use for
     # passive connections.  Decomment in case you're behind a NAT.
-    #handler.masquerade_address = '151.25.42.11'
-    #handler.passive_ports = range(60000, 65535)
+    # handler.masquerade_address = '151.25.42.11'
+    # handler.passive_ports = range(60000, 65535)
 
     # Instantiate FTP server class and listen on 0.0.0.0:2121
     address = ('', 2121)
-    server = FTPServer(address, handler)
+    server = VerboseServer(address, handler)
 
     # set a limit for connections
     server.max_cons = 256
@@ -35,6 +41,7 @@ def main():
 
     # start ftp server
     server.serve_forever()
+
 
 if __name__ == '__main__':
     main()
